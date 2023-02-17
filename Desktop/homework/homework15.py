4760c58d3ccf2c681819bfd51ca35bb97a40dc9f
anastasia berishvili <berishvilianastasia250@gmail.com>
2023-02-18 00:13:42 +0400

    this is my first commit

---
+++ Desktop/homework/homework15.py
@@ -0,0 +1,17 @@
+number1 = int(input("write number one: "))
+number2 = int(input("write number two: "))
+i = number1
+k = number1
+if number1 < number2:
+    while i < number2:
+        print(i)
+        i += 3
+
+elif number1 == number2:
+    print("both numbers are equal")
+elif number1 > number2:
+    while k > number2:
+        print(k)
+        k -=3
+else:
+    print("error")
